a3172182955f958d2e42765a330a68b552934573
Wanijya <wanijyabisen@gmail.com>
2025-11-18 15:08:30 +0530

    setup backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import FacialExpression from "./components/FacialExpression";
+import MoodSongs from "./components/MoodSongs";
 
 function App() {
   return (
     <>
       <FacialExpression />
+      <MoodSongs />
     </>
   );
 }
